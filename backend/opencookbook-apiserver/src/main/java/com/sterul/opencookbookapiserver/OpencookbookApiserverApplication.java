87535782fdf2c54e63c5d12ee6e07566ca382e7b
steve192 <stefan@sterul.com>
2021-01-02 15:29:07 +0100

    Initial commit

---
+++ backend/opencookbook-apiserver/src/main/java/com/sterul/opencookbookapiserver/OpencookbookApiserverApplication.java
@@ -0,0 +1,13 @@
+package com.sterul.opencookbookapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencookbookApiserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpencookbookApiserverApplication.class, args);
+	}
+
+}
---
+++ backend/opencookbook-apiserver/src/test/java/com/sterul/opencookbookapiserver/OpencookbookApiserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sterul.opencookbookapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpencookbookApiserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
