7f2e581a0688d3bfcc649c1c2da3de13299394f8
steve192 <stefan@sterul.com>
2021-11-14 19:32:19 +0100

    Add renew token api

--- src/main/java/com/sterul/opencookbookapiserver/controllers/UserController.java
+++ src/main/java/com/sterul/opencookbookapiserver/controllers/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,6 +51,11 @@ public class UserController {
 		final String token = jwtTokenUtil.generateToken(userDetails);
 
 		return ResponseEntity.ok(new UserLoginResponse(token));
+    }
+
+    @GetMapping("/renewToken")
+    public void renewToken() {
+        //TODO: Implement
     }
 
     private void login(String username, String password) throws Exception {
