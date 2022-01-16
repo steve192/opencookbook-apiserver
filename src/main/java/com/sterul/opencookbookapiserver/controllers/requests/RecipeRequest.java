2c86fa187edac3fbd00600c4bca00308ae315ab1
steve192 <stefan@sterul.com>
2022-01-17 00:00:55 +0100

    Fix: Recipe requests not de-serializable

--- src/main/java/com/sterul/opencookbookapiserver/controllers/requests/RecipeRequest.java
+++ src/main/java/com/sterul/opencookbookapiserver/controllers/requests/RecipeRequest.java
@@ -2,8 +2,10 @@ package com.sterul.opencookbookapiserver.controllers.requests;
 
 import com.sterul.opencookbookapiserver.controllers.dto.RecipeBaseDTO;
 
+import lombok.AllArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
+@AllArgsConstructor
 public class RecipeRequest extends RecipeBaseDTO {
 }
--- src/main/java/com/sterul/opencookbookapiserver/controllers/responses/RecipeResponse.java
+++ src/main/java/com/sterul/opencookbookapiserver/controllers/responses/RecipeResponse.java
@@ -2,9 +2,11 @@ package com.sterul.opencookbookapiserver.controllers.responses;
 
 import com.sterul.opencookbookapiserver.controllers.dto.RecipeBaseDTO;
 
+import lombok.AllArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
+@AllArgsConstructor
 public class RecipeResponse extends RecipeBaseDTO {
 
 }
