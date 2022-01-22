d187678158ee944b773df6779f46b1e158666ad9
steve192 <stefan@sterul.com>
2022-01-22 18:57:30 +0100

    switch post to get

--- src/main/java/com/sterul/opencookbookapiserver/services/recipeimport/recipescrapers/RecipeScraperServiceProxy.java
+++ src/main/java/com/sterul/opencookbookapiserver/services/recipeimport/recipescrapers/RecipeScraperServiceProxy.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 import com.sterul.opencookbookapiserver.configurations.OpencookbookConfiguration;
 
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +18,7 @@ public class RecipeScraperServiceProxy {
 
     public String scrapeRecipe(String url) throws IOException {
         var client = HttpClientBuilder.create().build();
-        var request = new HttpPost(
+        var request = new HttpGet(
                 opencookbookConfiguration.getRecipeScaperServiceUrl() + "/api/v1/scrape-recipe?url=" + url);
         var response = client.execute(request);
         return EntityUtils.toString(response.getEntity(), "UTF-8");
