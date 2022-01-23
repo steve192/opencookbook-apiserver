1567423035ca6ec4daae67e6300ca1ccfa553024
steve192 <stefan@sterul.com>
2022-01-23 02:47:41 +0100

    add tests

---
+++ src/test/java/com/sterul/opencookbookapiserver/unit/services/recipeimport/IngredientServiceTest.java
@@ -0,0 +1,45 @@
+package com.sterul.opencookbookapiserver.unit.services.recipeimport;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.sterul.opencookbookapiserver.entities.Ingredient;
+import com.sterul.opencookbookapiserver.repositories.IngredientRepository;
+import com.sterul.opencookbookapiserver.services.IngredientService;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class IngredientServiceTest {
+
+    @Autowired
+    private IngredientService cut;
+
+    @MockBean
+    private IngredientRepository ingredientRepository;
+
+    @Mock
+    private Ingredient mockIngredient;
+
+    @Test
+    void ingredientIsCreated() {
+        when(ingredientRepository.findByName(any())).thenReturn(null);
+        cut.createOrGetIngredient(mockIngredient);
+        verify(ingredientRepository, times(1)).save(mockIngredient);
+    }
+
+    @Test
+    void ingredientIsReused() {
+        when(ingredientRepository.findByName(any())).thenReturn(mockIngredient);
+        cut.createOrGetIngredient(mockIngredient);
+        verify(ingredientRepository, times(0)).save(mockIngredient);
+    }
+}
