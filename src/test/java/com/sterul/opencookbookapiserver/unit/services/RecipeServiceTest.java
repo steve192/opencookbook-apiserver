1d4fa68bc654360a8a6e29de53ee3f02f7d43fcb
steve192 <stefan@sterul.com>
2022-01-23 03:11:56 +0100

    Add recipe service tests

---
+++ src/test/java/com/sterul/opencookbookapiserver/unit/services/RecipeServiceTest.java
@@ -0,0 +1,60 @@
+package com.sterul.opencookbookapiserver.unit.services;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
+import com.sterul.opencookbookapiserver.entities.recipe.Recipe;
+import com.sterul.opencookbookapiserver.entities.recipe.RecipeGroup;
+import com.sterul.opencookbookapiserver.repositories.RecipeRepository;
+import com.sterul.opencookbookapiserver.services.RecipeGroupService;
+import com.sterul.opencookbookapiserver.services.RecipeService;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class RecipeServiceTest {
+
+    @Autowired
+    private RecipeService cut;
+
+    @MockBean
+    private RecipeRepository recipeRepository;
+    @MockBean
+    private RecipeGroupService recipeGroupService;
+
+    @Mock
+    private Recipe mockRecipe;
+    @Mock
+    private RecipeGroup mockRecipeGroupWithoutId;
+    @Mock
+    private RecipeGroup mockRecipeGroupWithId;
+
+    @Test
+    void recipeCreated() {
+        cut.createNewRecipe(mockRecipe);
+        verify(recipeRepository, times(1)).save(mockRecipe);
+    }
+
+    @Test
+    void recipeGroupCreatedIfNotExistent() {
+        when(mockRecipeGroupWithoutId.getId()).thenReturn(null);
+        when(mockRecipeGroupWithId.getId()).thenReturn(1L);
+        when(recipeGroupService.createRecipeGroup(any())).thenReturn(mockRecipeGroupWithId);
+        when(mockRecipe.getRecipeGroups()).thenReturn(Arrays.asList(mockRecipeGroupWithoutId));
+
+        cut.createNewRecipe(mockRecipe);
+
+        verify(recipeGroupService, times(1)).createRecipeGroup(mockRecipeGroupWithoutId);
+    }
+
+}
