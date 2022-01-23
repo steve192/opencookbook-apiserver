9f031444e07f2d96520489cbc0c8bc640a62b561
steve192 <stefan@sterul.com>
2022-01-23 03:12:04 +0100

    Move ingredient service tests

--- src/test/java/com/sterul/opencookbookapiserver/unit/services/IngredientServiceTest.java
+++ src/test/java/com/sterul/opencookbookapiserver/unit/services/IngredientServiceTest.java
@@ -1,4 +1,4 @@
-package com.sterul.opencookbookapiserver.unit.services.recipeimport;
+package com.sterul.opencookbookapiserver.unit.services;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
