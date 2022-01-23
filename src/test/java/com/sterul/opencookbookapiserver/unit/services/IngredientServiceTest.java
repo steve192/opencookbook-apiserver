5297c0bda361ff4611f91612efedf56cba6fc1da
steve192 <stefan@sterul.com>
2022-01-23 03:28:46 +0100

    Remove public modifiers from tests

--- src/test/java/com/sterul/opencookbookapiserver/unit/services/IngredientServiceTest.java
+++ src/test/java/com/sterul/opencookbookapiserver/unit/services/IngredientServiceTest.java
@@ -18,7 +18,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-public class IngredientServiceTest {
+class IngredientServiceTest {
 
     @Autowired
     private IngredientService cut;
