48df9eaa69f11119ee398bcebc0f9c02fea3ecaa
steve192 <stefan@sterul.com>
2022-03-20 15:36:39 +0100

    add check for active user

--- src/main/java/com/sterul/opencookbookapiserver/configurations/security/requestfilters/JwtRequestFilter.java
+++ src/main/java/com/sterul/opencookbookapiserver/configurations/security/requestfilters/JwtRequestFilter.java
@@ -1,14 +1,7 @@
 package com.sterul.opencookbookapiserver.configurations.security.requestfilters;
 
-import java.io.IOException;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.sterul.opencookbookapiserver.util.JwtTokenUtil;
-
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,7 +11,11 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import io.jsonwebtoken.ExpiredJwtException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
@@ -57,8 +54,6 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             if (!Boolean.TRUE.equals(jwtTokenUtil.validateToken(jwtToken, userDetails))) {
                 return;
             }
-
-
 
             UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
                     userDetails,
--- src/main/java/com/sterul/opencookbookapiserver/controllers/UserController.java
+++ src/main/java/com/sterul/opencookbookapiserver/controllers/UserController.java
@@ -2,9 +2,11 @@ package com.sterul.opencookbookapiserver.controllers;
 
 import com.sterul.opencookbookapiserver.controllers.exceptions.NotAuthorizedException;
 import com.sterul.opencookbookapiserver.controllers.exceptions.UnauthorizedException;
+import com.sterul.opencookbookapiserver.controllers.exceptions.UserNotActiveException;
 import com.sterul.opencookbookapiserver.controllers.requests.RefreshTokenRequest;
 import com.sterul.opencookbookapiserver.controllers.requests.UserCreationRequest;
 import com.sterul.opencookbookapiserver.controllers.requests.UserLoginRequest;
+import com.sterul.opencookbookapiserver.controllers.responses.LoginErrorResponse;
 import com.sterul.opencookbookapiserver.controllers.responses.RefreshTokenResponse;
 import com.sterul.opencookbookapiserver.controllers.responses.UserInfoResponse;
 import com.sterul.opencookbookapiserver.controllers.responses.UserLoginResponse;
@@ -18,10 +20,11 @@ import com.sterul.opencookbookapiserver.services.exceptions.ElementNotFound;
 import com.sterul.opencookbookapiserver.services.exceptions.InvalidActivationLinkException;
 import com.sterul.opencookbookapiserver.services.exceptions.UserAlreadyExistsException;
 import com.sterul.opencookbookapiserver.util.JwtTokenUtil;
-
-import lombok.SneakyThrows;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -29,9 +32,6 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 import javax.mail.MessagingException;
 
@@ -41,103 +41,110 @@ import javax.mail.MessagingException;
 @Slf4j
 public class UserController extends BaseController {
 
-	@Autowired
-	private AuthenticationManager authenticationManager;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-	@Autowired
-	private JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
-	@Autowired
-	private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@Autowired
-	private RefreshTokenService refreshTokenService;
+    @Autowired
+    private RefreshTokenService refreshTokenService;
 
-	@Autowired
-	private EmailService emailService;
+    @Autowired
+    private EmailService emailService;
 
-	@Operation(summary = "Creates a new user")
-	@PostMapping("/signup")
-	public User signup(@RequestBody UserCreationRequest userCreationRequest) throws UserAlreadyExistsException {
-		var createdUser = userService.createUser(userCreationRequest.getEmailAddress(), userCreationRequest.getPassword());
-		var activationLink = userService.createActivationLink(createdUser);
-		try {
-			emailService.sendActivationMail(activationLink);
-		} catch (MessagingException e) {
-			log.error("Error sending activation mail");
-		}
-		return createdUser;
-	}
+    @Operation(summary = "Creates a new user")
+    @PostMapping("/signup")
+    public User signup(@RequestBody UserCreationRequest userCreationRequest) throws UserAlreadyExistsException {
+        var createdUser = userService.createUser(userCreationRequest.getEmailAddress(), userCreationRequest.getPassword());
+        var activationLink = userService.createActivationLink(createdUser);
+        try {
+            emailService.sendActivationMail(activationLink);
+        } catch (MessagingException e) {
+            log.error("Error sending activation mail");
+        }
+        return createdUser;
+    }
 
-	@Operation(summary = "Logs a user in", description = "Logs in and generates tokens for authentication")
-	@PostMapping("/login")
-	public ResponseEntity<UserLoginResponse> login(@RequestBody UserLoginRequest authenticationRequest)
-			throws UnauthorizedException {
-		login(authenticationRequest.getEmailAddress(), authenticationRequest.getPassword());
+    @Operation(summary = "Logs a user in", description = "Logs in and generates tokens for authentication")
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody UserLoginRequest authenticationRequest)
+            throws UnauthorizedException {
 
-		final UserDetails userDetails = userDetailsService
-				.loadUserByUsername(authenticationRequest.getEmailAddress());
+        try {
+            login(authenticationRequest.getEmailAddress(), authenticationRequest.getPassword());
+        } catch (UserNotActiveException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(LoginErrorResponse.builder().error("NOT_ACTIVE").build());
+        }
 
-		final String token = jwtTokenUtil.generateToken(userDetails);
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getEmailAddress());
 
-		var response = new UserLoginResponse();
-		response.setToken(token);
-		response.setRefreshToken(refreshTokenService
-				.createRefreshTokenForUser(userService.getUserByEmail(userDetails.getUsername())).getToken());
+        final String token = jwtTokenUtil.generateToken(userDetails);
 
-		return ResponseEntity.ok(response);
-	}
+        var response = new UserLoginResponse();
+        response.setToken(token);
+        response.setRefreshToken(refreshTokenService
+                .createRefreshTokenForUser(userService.getUserByEmail(userDetails.getUsername())).getToken());
 
-	@Operation( summary = "Activates a user, using an activation id")
-	@GetMapping("/activate")
-	public void activateUser(@RequestParam String activationId) throws InvalidActivationLinkException {
-		userService.activateUser(activationId);
-	}
+        return ResponseEntity.ok(response);
+    }
 
-	@Operation(summary = "Get information about authenticated user account")
-	@GetMapping("/self")
-	public UserInfoResponse getOwnUserInfo() {
-		var response = new UserInfoResponse();
-		response.setEmail(getLoggedInUser().getEmailAddress());
-		return response;
-	}
+    @Operation(summary = "Activates a user, using an activation id")
+    @GetMapping("/activate")
+    public void activateUser(@RequestParam String activationId) throws InvalidActivationLinkException {
+        userService.activateUser(activationId);
+    }
 
-	@Operation(summary = "Delete authenticated user account")
-	@DeleteMapping("/self")
-	public void deleteOwnUser() {
-		userService.deleteUser(getLoggedInUser());
-	}
+    @Operation(summary = "Get information about authenticated user account")
+    @GetMapping("/self")
+    public UserInfoResponse getOwnUserInfo() {
+        var response = new UserInfoResponse();
+        response.setEmail(getLoggedInUser().getEmailAddress());
+        return response;
+    }
 
-	@Operation(summary = "Generate a JWT token from a refresh token", description = "The JWT token is used to authenticate against all apis using the \"Authentication: Bearer < token >\" header field")
-	@PostMapping("/refreshToken")
-	public RefreshTokenResponse renewToken(@RequestBody RefreshTokenRequest refreshTokenRequest)
-			throws NotAuthorizedException {
-		if (!refreshTokenService.isTokenValid(refreshTokenRequest.getRefreshToken())) {
-			throw new NotAuthorizedException();
-		}
-		RefreshToken refreshToken;
-		try {
-			refreshToken = refreshTokenService.getRefreshToken(refreshTokenRequest.getRefreshToken());
-		} catch (ElementNotFound e) {
-			throw new NotAuthorizedException();
-		}
-		var userDetails = userDetailsService.loadUserByUsername(refreshToken.getOwner().getEmailAddress());
-		var jwtToken = jwtTokenUtil.generateToken(userDetails);
+    @Operation(summary = "Delete authenticated user account")
+    @DeleteMapping("/self")
+    public void deleteOwnUser() {
+        userService.deleteUser(getLoggedInUser());
+    }
 
-		var response = new RefreshTokenResponse();
-		response.setToken(jwtToken);
-		return response;
-	}
+    @Operation(summary = "Generate a JWT token from a refresh token", description = "The JWT token is used to authenticate against all apis using the \"Authentication: Bearer < token >\" header field")
+    @PostMapping("/refreshToken")
+    public RefreshTokenResponse renewToken(@RequestBody RefreshTokenRequest refreshTokenRequest)
+            throws NotAuthorizedException {
+        if (!refreshTokenService.isTokenValid(refreshTokenRequest.getRefreshToken())) {
+            throw new NotAuthorizedException();
+        }
+        RefreshToken refreshToken;
+        try {
+            refreshToken = refreshTokenService.getRefreshToken(refreshTokenRequest.getRefreshToken());
+        } catch (ElementNotFound e) {
+            throw new NotAuthorizedException();
+        }
+        var userDetails = userDetailsService.loadUserByUsername(refreshToken.getOwner().getEmailAddress());
+        var jwtToken = jwtTokenUtil.generateToken(userDetails);
 
-	private void login(String username, String password) throws UnauthorizedException {
-		try {
-			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-		} catch (DisabledException | BadCredentialsException e) {
-			throw new UnauthorizedException();
-		}
-	}
+        var response = new RefreshTokenResponse();
+        response.setToken(jwtToken);
+        return response;
+    }
+
+    private void login(String username, String password) throws UnauthorizedException, UserNotActiveException {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (BadCredentialsException e) {
+            throw new UnauthorizedException();
+        } catch (DisabledException e) {
+            throw new UserNotActiveException();
+        }
+    }
 
 }
