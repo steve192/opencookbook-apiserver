47e1f6d0a160307693d7e6a3e240ff3a1168acfd
steve192 <stefan@sterul.com>
2022-03-20 16:31:03 +0100

    add tests

--- src/main/java/com/sterul/opencookbookapiserver/controllers/requests/UserCreationRequest.java
+++ src/main/java/com/sterul/opencookbookapiserver/controllers/requests/UserCreationRequest.java
@@ -1,8 +1,10 @@
 package com.sterul.opencookbookapiserver.controllers.requests;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UserCreationRequest {
     private String emailAddress;
     private String password;
--- src/test/java/com/sterul/opencookbookapiserver/integration/UserAPIIntegrationTest.java
+++ src/test/java/com/sterul/opencookbookapiserver/integration/UserAPIIntegrationTest.java
@@ -1,31 +1,41 @@
 package com.sterul.opencookbookapiserver.integration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-
 import com.sterul.opencookbookapiserver.controllers.UserController;
+import com.sterul.opencookbookapiserver.controllers.exceptions.UnauthorizedException;
+import com.sterul.opencookbookapiserver.controllers.requests.UserCreationRequest;
+import com.sterul.opencookbookapiserver.controllers.requests.UserLoginRequest;
 import com.sterul.opencookbookapiserver.entities.account.User;
 import com.sterul.opencookbookapiserver.repositories.UserRepository;
-
+import com.sterul.opencookbookapiserver.services.EmailService;
+import com.sterul.opencookbookapiserver.services.exceptions.UserAlreadyExistsException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.mail.MessagingException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserAPIIntegrationTest {
 
+    final String testPassword = "12345";
     @MockBean
     UserRepository userRepository;
-
+    @MockBean
+    EmailService emailService;
     @Autowired
     UserController cut;
-
     User testUser;
 
     @BeforeEach
@@ -46,6 +56,26 @@ class UserAPIIntegrationTest {
     @Test
     void testUserInfo() {
         assertEquals(cut.getOwnUserInfo().getEmail(), testUser.getEmailAddress());
+    }
+
+    @Test
+    @Transactional
+    void registrationEmailSent() throws UserAlreadyExistsException, MessagingException {
+        cut.signup(UserCreationRequest.builder().emailAddress("testi@cookpal.io").password("12345").build());
+        verify(emailService, times(1)).sendActivationMail(any());
+    }
+
+    @Test
+    @Transactional
+    void nonActivatedUserCannotLogin() throws UserAlreadyExistsException, UnauthorizedException {
+        var response = cut.login(UserLoginRequest.builder()
+                .emailAddress(testUser.getEmailAddress())
+                .password(testPassword)
+                .build());
+
+        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+        assertTrue(response.getBody().toString().contains("NOT_ACTIVE"));
+
     }
 
 }
