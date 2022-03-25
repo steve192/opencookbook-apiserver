e734d7255af85dff63f7275c7174e2605284ef18
steve192 <stefan@sterul.com>
2022-03-25 14:03:26 +0100

    fix unhandled error when recipe scrapers does not support given website

--- src/main/java/com/sterul/opencookbookapiserver/services/recipeimport/recipescrapers/RecipeScraperServiceProxy.java
+++ src/main/java/com/sterul/opencookbookapiserver/services/recipeimport/recipescrapers/RecipeScraperServiceProxy.java
@@ -1,9 +1,8 @@
 package com.sterul.opencookbookapiserver.services.recipeimport.recipescrapers;
 
-import java.io.IOException;
-
 import com.sterul.opencookbookapiserver.configurations.OpencookbookConfiguration;
-
+import com.sterul.opencookbookapiserver.services.recipeimport.ImportNotSupportedException;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -12,23 +11,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 public class RecipeScraperServiceProxy {
 
     @Autowired
     OpencookbookConfiguration opencookbookConfiguration;
 
-    private CloseableHttpClient client;
+    private final CloseableHttpClient client;
 
     public RecipeScraperServiceProxy() {
         client = HttpClientBuilder.create().build();
     }
 
-    public String scrapeRecipe(String url) throws IOException {
+    public String scrapeRecipe(String url) throws IOException, ImportNotSupportedException {
 
         var request = new HttpGet(
                 opencookbookConfiguration.getRecipeScaperServiceUrl() + "/api/v1/scrape-recipe?url=" + url);
         var response = client.execute(request);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_NOT_IMPLEMENTED) {
+            throw new ImportNotSupportedException();
+        }
         return EntityUtils.toString(response.getEntity(), "UTF-8");
     }
 
