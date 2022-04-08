10575d1e7bcf4c4da362328307179c2762cdb994
steve192 <stefan@sterul.com>
2022-04-09 01:49:54 +0200

    fix: http connection not closed

--- src/main/java/com/sterul/opencookbookapiserver/services/recipeimport/recipescrapers/RecipeScraperServiceProxy.java
+++ src/main/java/com/sterul/opencookbookapiserver/services/recipeimport/recipescrapers/RecipeScraperServiceProxy.java
@@ -5,7 +5,7 @@ import com.sterul.opencookbookapiserver.services.recipeimport.ImportNotSupported
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
@@ -19,27 +19,25 @@ public class RecipeScraperServiceProxy {
     @Autowired
     OpencookbookConfiguration opencookbookConfiguration;
 
-    private final CloseableHttpClient client;
-
-    public RecipeScraperServiceProxy() {
-        client = HttpClientBuilder.create().build();
-    }
-
     public String scrapeRecipe(String url) throws IOException, ImportNotSupportedException {
 
         var request = new HttpGet(
                 opencookbookConfiguration.getRecipeScaperServiceUrl() + "/api/v1/scrape-recipe?url=" + url);
-        var response = client.execute(request);
-        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_NOT_IMPLEMENTED) {
-            throw new ImportNotSupportedException();
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            var response = httpclient.execute(request);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_NOT_IMPLEMENTED) {
+                throw new ImportNotSupportedException();
+            }
+            return EntityUtils.toString(response.getEntity(), "UTF-8");
         }
-        return EntityUtils.toString(response.getEntity(), "UTF-8");
     }
 
     @Cacheable("recipe_scrapers_supported_hosts")
     public String getSupportedHosts() throws IOException {
-        var response = client.execute(new HttpGet(
-                opencookbookConfiguration.getRecipeScaperServiceUrl() + "/api/v1/scrape-recipe/supported-hosts"));
-        return EntityUtils.toString(response.getEntity(), "UTF-8");
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            var response = httpclient.execute(new HttpGet(
+                    opencookbookConfiguration.getRecipeScaperServiceUrl() + "/api/v1/scrape-recipe/supported-hosts"));
+            return EntityUtils.toString(response.getEntity(), "UTF-8");
+        }
     }
 }
