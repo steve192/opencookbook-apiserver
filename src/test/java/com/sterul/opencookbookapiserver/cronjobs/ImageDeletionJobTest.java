9010537660848d5b83fa779cf7dcfc458dde4505
steve192 <stefan@sterul.com>
2022-11-29 20:06:45 +0100

    Prepare image deletion job

---
+++ src/test/java/com/sterul/opencookbookapiserver/cronjobs/ImageDeletionJobTest.java
@@ -0,0 +1,11 @@
+package com.sterul.opencookbookapiserver.cronjobs;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class ImageDeletionJobTest {
+    // TODO: Test
+
+}
