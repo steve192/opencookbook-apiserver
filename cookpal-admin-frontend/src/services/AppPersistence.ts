ca78cb2dbfefb65e8088b8e8cdbdf414a5a2cb49
steve192 <stefan@sterul.com>
2025-06-09 03:32:35 +0200

    Persist session

--- cookpal-admin-frontend/src/services/AppPersistence.ts
+++ cookpal-admin-frontend/src/services/AppPersistence.ts
@@ -1,25 +1,30 @@
-
 export default class AppPersistence {
   private static authToken: string;
   private static refreshToken: string;
 
   static async setAuthToken(token: string) {
     AppPersistence.authToken = token;
+    localStorage.setItem('authToken', token); // Save to localStorage
   }
 
   static async getAuthToken(): Promise<string | null> {
+    if (!AppPersistence.authToken) {
+      AppPersistence.authToken = localStorage.getItem('authToken') || ''; // Retrieve from localStorage
+    }
     return AppPersistence.authToken;
   }
 
-
   static async setRefreshToken(token: string) {
     AppPersistence.refreshToken = token;
+    localStorage.setItem('refreshToken', token); // Save to localStorage
   }
 
   static async getRefreshToken(): Promise<string | null> {
+    if (!AppPersistence.refreshToken) {
+      AppPersistence.refreshToken = localStorage.getItem('refreshToken') || ''; // Retrieve from localStorage
+    }
     return AppPersistence.refreshToken;
   }
-
 
   static async getBackendURL(): Promise<string> {
     // return 'https://beta.cookpal.io';
